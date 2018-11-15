261bea5db529400083d745f9f66bc73425330302
755307360@qq.com <755307360@qq.com>
2018-11-15 15:57:53 +0800

    伟分别比别的地方

---
+++ yu/src/com/jh/Test.java
@@ -0,0 +1,11 @@
+package com.jh;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         String str="123";
+         System.out.println(str);
+	}
+
+}
