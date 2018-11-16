6ab922359fd40018478fb62d27080d6e7606bcc2
755307360@qq.com <755307360@qq.com>
2018-11-16 10:03:08 +0800

    123

--- yu/src/com/jh/Test.java
+++ yu/src/com/jh/Test.java
@@ -5,7 +5,9 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
          String str="123";
+         System.out.println();
          System.out.println(str);
+         
 	}
 
 }
